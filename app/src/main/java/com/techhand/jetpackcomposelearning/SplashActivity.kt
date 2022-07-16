75c93ede8cd9d2147be09611bee7a0336c7f9f04
dev-nick-patel <nikunjpatel.au@gmail.com>
2022-07-16 18:02:28 +1000

    Modifiers- Create a Image card with title

--- app/src/main/java/com/techhand/jetpackcomposelearning/SplashActivity.kt
+++ app/src/main/java/com/techhand/jetpackcomposelearning/SplashActivity.kt
@@ -3,53 +3,84 @@ package com.techhand.jetpackcomposelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.techhand.jetpackcomposelearning.ui.theme.JetpackComposeLearningTheme
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Column() or Row()
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Green),
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.SpaceBetween, // make the even space between the component/view
-            /*
-            Arrangement.SpaceEvenly // Make the same space between the all views in vertical
-            Arrangement.SpaceAround //
-             */
-            ) {
-                Text(text = "Hello")
-                Text(text = "World")
-                Text(text = "Nikunj")
-            }
-            
-            Row(
-                modifier = Modifier
-                    .width(200.dp)
-                    .fillMaxHeight()
-                    .background(Color.Red),
-
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Nikunj")
-                Text(text = "Patel")
+            val painter = painterResource(id = R.drawable.cartoon)
+            val description = "Cartoon Image"
+            val title = "Cartoon is Playing"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)){
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
 
         }
     }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                ))
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+
 }
 
 @Composable
